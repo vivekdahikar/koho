1efb0968432bd80653c71e762087885dbab1595e
V1V320 <dahikarvivek27@gmail.com>
2023-03-16 10:03:48 +0530

    updated customizer

--- src/app/shared/components/customizer/color/color.component.ts
+++ src/app/shared/components/customizer/color/color.component.ts
@@ -11,19 +11,16 @@ export class ColorComponent {
   public MIXLayout: string = 'default';
   @HostBinding('@.disabled')
   public selectedValue: any;
-  public primary_color: string = '#6362E7';
-  public secondary_color: string = '#FFC500';
+  public primary_color: string = '#24695c';
+  public secondary_color: string = '#ba895d';
 
-  constructor(
-    public layout: LayoutService,
-    @Inject(DOCUMENT) private document: Document
-  ) {}
+  constructor(public layout: LayoutService) {}
 
   ngOnInit(): void {}
 
   applyColor() {
     document.documentElement.style.setProperty(
-      '--theme-deafult',
+      '--theme-default',
       this.primary_color
     );
     document.documentElement.style.setProperty(
@@ -33,8 +30,27 @@ export class ColorComponent {
     this.layout.config.color.primary_color = this.primary_color;
     this.layout.config.color.secondary_color = this.secondary_color;
   }
+  resetColor() {
+    document.documentElement.style.setProperty('--theme-default', '#24695c');
+    document.documentElement.style.setProperty('--theme-secondary', '#ba895d');
+    (<HTMLInputElement>document.getElementById('ColorPicker1')).value =
+      '#24695c';
+    (<HTMLInputElement>document.getElementById('ColorPicker2')).value =
+      '#ba895d';
+    this.layout.config.color.primary_color = '#24695c';
+    this.layout.config.color.secondary_color = '#ba895d';
+  }
 
   customizeMixLayout(val: any) {
-    this.document.body.classList.toggle('dark-only');
+    this.MIXLayout = val;
+    this.layout.config.settings.layout_version = val;
+    document.body?.classList.remove('light-only', 'dark-sidebar', 'dark-only');
+    if (val === 'default') {
+      document.body?.classList.add('light-only');
+    } else if (val === 'dark-sidebar') {
+      document.body?.classList.add('dark-sidebar');
+    } else {
+      document.body?.classList.add('dark-only');
+    }
   }
 }
