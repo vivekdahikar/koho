ed559929eca79f6ab3cda1db3da294e1d7109ee0
DaxitaWebiots <daxita.rafaliya@webiots.com>
2023-03-29 17:37:05 +0530

    applications pages

--- src/app/shared/data/bookmark/bookmark.ts
+++ src/app/shared/data/bookmark/bookmark.ts
@@ -1,34 +1,44 @@
 export const bookmarks = [
   {
-    id: 1,
-    img: 'assets/images/lightgallry/01.jpg',
-    url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
-    title: 'Admin Template',
-    collection: 'General',
-    value: 'Admin Template',
+     id: 1,
+     img: 'assets/images/lightgallry/01.jpg',
+     url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
+     title: 'Admin Template',
+     collection: 'General'
   },
   {
-    id: 2,
-    img: 'assets/images/lightgallry/02.jpg',
-    url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
-    title: 'Universal Template',
-    collection: 'General',
-    value: 'Universal Template',
+     id: 2,
+     img: 'assets/images/lightgallry/02.jpg',
+     url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
+     title: 'Universal Template',
+     collection: 'General'
   },
   {
-    id: 3,
-    img: 'assets/images/lightgallry/03.jpg',
-    url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
-    title: 'Angular Theme',
-    collection: 'Fs',
-    value: 'Angular Template',
+     id: 3,
+     img: 'assets/images/lightgallry/03.jpg',
+     url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
+     title: 'Angular Theme',
+     collection: 'Fs'
   },
   {
-    id: 4,
-    img: 'assets/images/lightgallry/04.jpg',
-    url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
-    title: 'Multikart Admin',
-    collection: 'General',
-    value: 'Multikart Template',
+     id: 4,
+     img: 'assets/images/lightgallry/04.jpg',
+     url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
+     title: 'Multikart Admin',
+     collection: 'General'
   },
-];
+  {
+     id: 5,
+     img: 'assets/images/lightgallry/05.jpg',
+     url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
+     title: 'Ecommerece theme',
+     collection: 'Fs'
+  },
+  {
+     id: 6,
+     img: 'assets/images/lightgallry/06.jpg',
+     url: 'http://admin.pixelstrap.com/endless/ltr/landing-page.html',
+     title: 'Tovo app landing page',
+     collection: 'General'
+  }
+]
